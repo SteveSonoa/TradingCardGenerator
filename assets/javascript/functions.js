d41e3c10ec533339071ca9a1cce212cf81dda8d2
Steve Marshall <stevesonoa@gmail.com>
2017-12-11 12:01:06 -0700

    pseudocoded functions drawMainPage and displayFinishedCards

---
+++ assets/javascript/functions.js
@@ -0,0 +1,29 @@
+function drawMainPage() {
+	// col-lg-3 col-md-3 col-sm-12 col-xs-12 Add Logo
+	// col-lg-9 col-md-9 col-sm-12 col-xs-12 "Create your own trading cards!"
+	// row
+	// Step 1: Pick at basketball player.
+	// Input area, id=playerSearch
+	// Submit button, id=Submit
+}
+
+function displayFinishedCards() {
+	// col-12 Click to flip over
+	// col-12 Front of image (flip)
+	// on click...
+		// flip to close front of image
+		// flip to open back of image
+	// Save Front button // Save Back button
+	// if user is logged in
+		// while more images remain in user's search history
+			// col-12 My Collection
+			// col-3 thumbnail of next image
+			// click to put thumbnail in main image spot
+			// ALL images will be displayed in the thumbnail images - including the main images, even if just 1
+	// load stories (limit 10)
+		// images(float: left)
+		// headline
+		// summary
+		// Read on ESPN.com...
+		// Divider
+}
