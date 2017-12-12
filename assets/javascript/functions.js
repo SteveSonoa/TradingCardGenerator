43631bfa826cbc9b985fa6935e7ddc03bb7dfe33
Steve Marshall <stevesonoa@gmail.com>
2017-12-12 16:16:41 -0700

    updated functions.js with complete drawMainPage(), complete drawLogo(), complete searchForPlayer(), partial displayFinishedCards, partial displayFocusCard, and partial displayStories.

--- assets/javascript/functions.js
+++ assets/javascript/functions.js
@@ -1,7 +1,8 @@
 // Creates the layout of the Main Page when a user first accesses the site
-function drawMainPage() {
-	$("#mainDiv").html(
-		'<div col="row">'
+function drawMainPage(introDiv) {
+	var myLocation = "#" + introDiv;
+	$(myLocation).html(
+	+	'<div col="row">'
 	+		'<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12" id="mainText">'
 	+			'<div><h1 class="text-center">Create your own</h1></div>'
 	+			'<div><h1 class="text-center">CUSTOMIZED</h1></div>'
@@ -44,13 +45,20 @@ function searchForPlayer(searchDiv) {
 }
 
 // Draw the card collections available to the player, including the ability to flip it over from front to back
-function displayFinishedCards() {
-	// col-12 Click to flip over
-	// col-12 Front of image (flip)
-	// on click...
-		// flip to close front of image
-		// flip to open back of image
-	// Save Front button // Save Back button
+function displayFinishedCards(libraryDiv) {
+	var myLocation = "#" + libraryDiv;
+	$(myLocation).html(
+	+	'<div col="row">'
+	+		'<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="focusCardHeader"><h1>Click To Flip This Card Over</h1></div>'
+	+	'</div>'
+	+	'<div col="row">'
+	+		'<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="focusCard"></div>'
+	+	'</div>'
+	+	'<div col="row">'
+	+		'<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="saveOptions">Save Card Front // Save Card Back</div>'
+	+	'</div>'
+	);
+	displayFocusCard("focusCard");
 	// if user is logged in
 		// while more images remain in user's search history
 			// col-12 My Collection
@@ -59,8 +67,25 @@ function displayFinishedCards() {
 			// ALL images will be displayed in the thumbnail images - including the main images, even if just 1
 }
 
+// Draw the main focus card & program the flip
+function displayFocusCard(mainCardDiv) {
+	var myLocation = "#" + mainCardDiv;
+	// Front of image (flip)
+	// on click...
+		// flip to close front of image
+		// flip to open back of image
+}
+
 // Load news stories about the player & his team
-function displayStories() {
+function displayStories(storyDiv) {
+	var myLocation = "#" + storyDiv;
+	$(myLocation).append(
+	+	'<div col="row">'
+	+		'<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12" id="mainText">'
+	+		'</div>'
+	+		'<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12" id="bodyLogo"></div>'
+	+	'</div>'
+	);
 	// load stories (limit 10)
 		// images(float: left)
 		// headline
