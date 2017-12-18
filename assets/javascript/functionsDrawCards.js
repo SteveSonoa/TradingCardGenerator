41f984f0a38d9b2907f984835d561e1a0ba646e2
Steve Marshall <stevesonoa@gmail.com>
2017-12-17 19:21:38 -0700

    Added card display functionality when thumbnail is clicked.

--- assets/javascript/functionsDrawCards.js
+++ assets/javascript/functionsDrawCards.js
@@ -3,12 +3,13 @@ function drawCollegeFront(divName) {
 	$(myLocation).html(
 		'<div class="container-float cardTemplateURL" id="templateArea">'
 	+		'<div class="row">'
-	+			'<div class="col-lg-12" id="templateImage"><img src="TradingCardGenerator/assets/images/templateCollegeFront.png" height="400" width="285" />'
+	+			'<div class="col-lg-12" id="templateImage"><img src="assets/images/templateCollegeFront.png" height="400" width="285" />'
 	+				'<div class="templateElements text-uppercase" id="CollegeFrontName">' + characterName + '</div>'
 	+			'</div>'
 	+		'</div>'
 	+	'</div>'
 	);
+	$("#templateImage").append('<img src="' + ImageUrl + '" class="myGiphyImg" height="400" width="285" />');
 }
 
 function drawCollegeBack(divName) {
@@ -16,7 +17,7 @@ function drawCollegeBack(divName) {
 	$(myLocation).html(
 		'<div class="container-float cardTemplateURL" id="templateArea">'
 	+		'<div class="row">'
-	+			'<div class="col-lg-12" id="templateImage"><img src="TradingCardGenerator/assets/images/templateCollegeBack.png" height="400" width="285" />'
+	+			'<div class="col-lg-12" id="templateImage"><img src="assets/images/templateCollegeBack.png" height="400" width="285" />'
 	+				'<div class="templateElements text-uppercase" id="CollegeBackName">' + characterName + '</div>'
 	+				'<div class="templateElements" id="CollegeBackBio">' + swapistatsURL + '</div>'
 	+				'<div class="templateElements text-uppercase" id="CollegeBackImage"><img src="' + ImageUrl + '" class="img img-responsive" height="98" width="70" /></div>'
@@ -31,12 +32,14 @@ function drawSimpleFront(divName) {
 	$(myLocation).html(
 		'<div class="container-float cardTemplateURL" id="templateArea">'
 	+		'<div class="row">'
-	+			'<div class="col-lg-12" id="templateImage"><img src="TradingCardGenerator/assets/images/templateSimpleFront.png" height="400" width="285" />'
+	+			'<div class="col-lg-12" id="templateImage"><img src="assets/images/templateSimpleFront.png" height="400" width="285" />'
 	+				'<div class="templateElements text-uppercase" id="SimpleFrontName">' + characterName + '</div>'
 	+			'</div>'
 	+		'</div>'
 	+	'</div>'
 	);
+	$("#templateImage").append('<img src="' + ImageUrl + '" class="myGiphyImg" height="400" width="285" />');
+
 }
 
 function drawSimpleBack(divName) {
@@ -44,7 +47,7 @@ function drawSimpleBack(divName) {
 	$(myLocation).html(
 		'<div class="container-float cardTemplateURL" id="templateArea">'
 	+		'<div class="row">'
-	+			'<div class="col-lg-12" id="templateImage"><img src="TradingCardGenerator/assets/images/templateSimpleBack.png" height="400" width="285" />'
+	+			'<div class="col-lg-12" id="templateImage"><img src="assets/images/templateSimpleBack.png" height="400" width="285" />'
 	+				'<div class="templateElements text-uppercase" id="SimpleBackName">' + characterName + '</div>'
 	+				'<div class="templateElements" id="SimpleBackBio">' + swapistatsURL + '</div>'
 	+				'<div class="templateElements text-uppercase" id="SimpleBackImage"><img src="' + ImageUrl + '" class="img img-responsive" height="165" width="118" /></div>'
@@ -59,12 +62,13 @@ function drawFutureFront(divName) {
 	$(myLocation).html(
 		'<div class="container-float cardTemplateURL" id="templateArea">'
 	+		'<div class="row">'
-	+			'<div class="col-lg-12" id="templateImage"><img src="TradingCardGenerator/assets/images/templateFutureFront.png" height="400" width="285" />'
+	+			'<div class="col-lg-12" id="templateImage"><img src="assets/images/templateFutureFront.png" height="400" width="285" />'
 	+				'<div class="templateElements text-uppercase" id="FutureFrontName">' + characterName + '</div>'
 	+			'</div>'
 	+		'</div>'
 	+	'</div>'
 	);
+	$("#templateImage").append('<img src="' + ImageUrl + '" class="myGiphyImg" height="400" width="285" />');
 }
 
 function drawFutureBack(divName) {
@@ -72,7 +76,7 @@ function drawFutureBack(divName) {
 	$(myLocation).html(
 		'<div class="container-float cardTemplateURL" id="templateArea">'
 	+		'<div class="row">'
-	+			'<div class="col-lg-12" id="templateImage"><img src="TradingCardGenerator/assets/images/templateFutureBack.png" height="400" width="285" />'
+	+			'<div class="col-lg-12" id="templateImage"><img src="assets/images/templateFutureBack.png" height="400" width="285" />'
 	+				'<div class="templateElements text-uppercase" id="FutureBackName">' + characterName + '</div>'
 	+				'<div class="templateElements" id="FutureBackBio">' + swapistatsURL + '</div>'
 	+				'<div class="templateElements text-uppercase" id="FutureBackImage"><img src="' + ImageUrl + '" class="img img-responsive" height="162" width="116" /></div>'
